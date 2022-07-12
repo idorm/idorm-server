5f26b5a8de073be8f22e789f7825a3f3dd854850
NaHyeon <nahyeonee99@gmail.com>
2022-07-12 14:59:14 +0900

    initial commit

---
+++ src/main/java/idorm/idormServer/IdormServerApplication.java
@@ -0,0 +1,13 @@
+package idorm.idormServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdormServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdormServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idorm/idormServer/IdormServerApplicationTests.java
@@ -0,0 +1,13 @@
+package idorm.idormServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdormServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
