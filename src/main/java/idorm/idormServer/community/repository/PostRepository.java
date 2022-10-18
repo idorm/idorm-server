a07a0e41a151b21275a047716ddcd3b024517dd3
nahyeon99 <nahyeonee99@gmail.com>
2022-10-19 02:12:26 +0900

    [Feat] Community repository 구현

--- src/main/java/idorm/idormServer/community/repository/PostRepository.java
+++ src/main/java/idorm/idormServer/community/repository/PostRepository.java
@@ -2,7 +2,16 @@ package idorm.idormServer.community.repository;
 
 import idorm.idormServer.community.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @Query(value = "SELECT post_id " +
+            "FROM post p " +
+            "WHERE p.dorm_num = :dormNum AND " +
+            "p.is_visible = 1", nativeQuery = true)
+    List<Long> findManyByDormCategory(@Param("dormNum") String dormNum);
 }
