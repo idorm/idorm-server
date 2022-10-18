fdeeafcc593b570e0104d142d62dbcbe9c0133b7
nahyeon99 <nahyeonee99@gmail.com>
2022-10-19 02:13:25 +0900

    [Feat] Community 게시글 업로드 관련 dto와 vo 구현

--- src/main/java/idorm/idormServer/community/dto/post/PostDefaultResponseDto.java
+++ src/main/java/idorm/idormServer/community/dto/post/PostDefaultResponseDto.java
@@ -1,2 +1,78 @@
-package idorm.idormServer.community.dto.post;public class PostDefaultResponseDto {
+package idorm.idormServer.community.dto.post;
+
+import idorm.idormServer.community.domain.Comment;
+import idorm.idormServer.community.domain.Post;
+import idorm.idormServer.member.domain.Member;
+import idorm.idormServer.photo.domain.Photo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ApiModel(value = "Post 응답")
+public class PostDefaultResponseDto {
+
+    @ApiModelProperty(position = 1, value="게시글 식별자")
+    private Long postId;
+
+    @ApiModelProperty(position = 2, value="작성자 식별자")
+    private Long memberId;
+
+    @ApiModelProperty(position = 3, value = "커뮤니티 카테고리")
+    private String dormNum;
+
+    @ApiModelProperty(position = 4, value = "게시글 제목")
+    private String title;
+
+    @ApiModelProperty(position = 5, value = "게시글 내용")
+    private String content;
+
+    @ApiModelProperty(position = 6, value = "게시글 익명 여부")
+    private Boolean isAnonymous;
+
+    @ApiModelProperty(position = 7, value = "게시글 공개 여부")
+    private Boolean isVisible;
+
+    @ApiModelProperty(position = 8, value = "업로드 사진 url")
+    private List<String> photoUrls = new ArrayList<>();
+
+//    @ApiModelProperty(position = 9, value = "공감한 멤버 식별자")
+//    private List<Long> postLikedMemberIds = new ArrayList<>();
+//
+//    @ApiModelProperty(position = 10, value = "댓글 식별자")
+//    private List<Long> commentIds = new ArrayList<>();
+
+    public PostDefaultResponseDto(Post post) {
+        this.postId = post.getId();
+        this.memberId = post.getMember().getId();
+        this.dormNum = post.getDormNum();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.isAnonymous = post.getIsAnonymous();
+        this.isVisible = post.getIsVisible();
+
+        if(post.getPhotos() != null) {
+            for(Photo photo : post.getPhotos()) {
+                this.photoUrls.add(photo.getUrl());
+            }
+        }
+
+//        if(post.getLikes() != null) {
+//            for(Member member : post.getLikes()) {
+//                this.postLikedMemberIds.add(member.getId());
+//            }
+//        }
+//
+//        if(post.getComments() != null) {
+//            for(Comment comment : post.getComments()) {
+//                this.commentIds.add(comment.getId());
+//            }
+//        }
+    }
 }
--- src/main/java/idorm/idormServer/community/vo/SavePostVo.java
+++ src/main/java/idorm/idormServer/community/vo/SavePostVo.java
@@ -1,2 +1,39 @@
-package idorm.idormServer.community;public class SavePostVo {
+package idorm.idormServer.community.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Schema(title = "게시글 저장 VO")
+public class SavePostVo {
+
+    @ApiModelProperty(position = 1, required = true, dataType = "Long", value = "멤버 식별자", example = "2")
+    @Schema(title = "멤버 식별자")
+    private Long memberId;
+
+    @NotBlank(message = "기숙사 분류를 입력해 주세요.")
+    @ApiModelProperty(position = 2, required = true, dataType = "String", value = "커뮤니티 기숙사 분류", example = "DORM1")
+    @Schema(title = "기숙사 분류")
+    private String dormNum;
+
+    @NotBlank(message = "제목을 입력해 주세요.")
+    @ApiModelProperty(position = 3, required = true, dataType = "String", value = "제목", example = "제에목")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해 주세요.")
+    @ApiModelProperty(position = 4, required = true, dataType = "String", value = "내용", example = "내애용")
+    private String content;
+
+    @ApiModelProperty(position = 5, required = true, dataType = "Boolean", value = "익명 여부")
+    private Boolean isAnonymous;
+
+    @ApiModelProperty(position = 6, dataType = "List<MultipartFile>", value = "게시글 사진들", example = "DORM1, DORM2, DORM3")
+    private List<MultipartFile> files = new ArrayList<>();
+
 }
