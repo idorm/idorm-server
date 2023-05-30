cc3727f829169ec805923c6355529e644859d88c
nahyeon99 <nahyeonee99@gmail.com>
2023-05-30 21:36:16 +0900

    [Fix] 일정 관리자용 응답 dto 오류 수정

--- src/main/java/idorm/idormServer/calendar/dto/CalendarAdminResponseDto.java
+++ src/main/java/idorm/idormServer/calendar/dto/CalendarAdminResponseDto.java
@@ -2,11 +2,20 @@ package idorm.idormServer.calendar.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import idorm.idormServer.calendar.domain.Calendar;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ApiModel(value = "Calendar 관리자용 응답")
 public class CalendarAdminResponseDto {
 
     @ApiModelProperty(position = 1, required = true, value= "캘린더 식별자")
