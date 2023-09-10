cfb995ed2264280d6d30e7c6f1b7f1816b6b6c5e
nahyeon99 <nahyeonee99@gmail.com>
2023-09-11 04:23:49 +0900

    [Refactor] 로깅 코드 개선 및 범위 제한 - servlet 요청의 경우 파라미터 로깅 제거 - 성공 응답의 비지니스 로직 로깅 제거

--- src/main/java/idorm/idormServer/application/LoggingAdvice.java
+++ src/main/java/idorm/idormServer/application/LoggingAdvice.java
@@ -4,15 +4,12 @@ import idorm.idormServer.exception.CustomException;
 import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
 
 import java.lang.reflect.Method;
 
-import static idorm.idormServer.exception.ExceptionCode.*;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
@@ -20,112 +17,68 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @Component
 public class LoggingAdvice {
 
-    @Pointcut("execution(* idorm.idormServer..*Controller.*(..))")
-    private void methodFromController(){}
-
-    @Pointcut("execution(public * idorm.idormServer..*(..))")
-    private void all() {
+    // TODO: Pointcut 범위 재설정
+    @Pointcut("execution(* idorm.idormServer.calendar..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.calendar..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.calendar..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.community..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.community..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.community..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.fcm..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.fcm..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.fcm..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.matching..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.matching..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.matching..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.matching..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.matching..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.matching..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.member..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.member..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.member..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.photo..*Controller.*(..)) || " +
+            "execution(* idorm.idormServer.photo..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.photo..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.report..*Repository.*(..)) || " +
+            "execution(* idorm.idormServer.report..*Service.*(..)) || " +
+            "execution(* idorm.idormServer.report..*Controller.*(..))")
+    private void allFromBusinessLogic() {
     }
 
-    @Before("all()")
-    public void logBeforeStart(JoinPoint joinPoint) {
-
-        try {
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-
-            String className = signature.getDeclaringType().getSimpleName();
-            Method method = signature.getMethod();
-
-            String[] parameterNames = signature.getParameterNames();
-            Object[] args = joinPoint.getArgs();
-
-            String returnArgs = null;
-
-            for (int i = 0; i < method.getParameters().length; i++) {
-                if (parameterNames[i] != null) {
-                    returnArgs += " | " + parameterNames[i] + " = ";
-                    returnArgs += args[i] + "  ";
-                }
-            }
-            log.info("[START] {} | {} {}", className, method.getName(), returnArgs);
-        } catch (Exception e) {
-            throw new CustomException(e, AOP_LOGGING_ERROR);
-        }
-    }
-
-    @AfterReturning(value = "all()", returning = "result")
-    public void logAfterSuccess(JoinPoint joinPoint, Object result) {
-
-        try {
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-
-            String className = signature.getDeclaringType().getSimpleName();
-            Method method = signature.getMethod();
-
-            log.info("[SUCCESS] {} | {} | return = {}", className, method.getName(), result);
-        } catch (Exception e) {
-            throw new CustomException(e, AOP_LOGGING_ERROR);
-        }
-    }
-
-    @Around("methodFromController()")
-    public Object logEndpoint(ProceedingJoinPoint joinPoint) throws Throwable {
+    @AfterThrowing(value = "allFromBusinessLogic()", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, CustomException exception) {
 
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
         String className = signature.getDeclaringType().getSimpleName();
         Method method = signature.getMethod();
 
-        log.info("========= [API START] {} {} API =========", className, method.getName());
-        StopWatch stopWatch = new StopWatch();
+        String[] parameterNames = signature.getParameterNames();
+        Object[] args = joinPoint.getArgs();
 
-        stopWatch.start();
-        Object proceed = joinPoint.proceed();
-        stopWatch.stop();
+        StringBuilder returnArgs = new StringBuilder();
 
-        long totalTimeMillis = stopWatch.getTotalTimeMillis();
-
-        log.info("========= [API FINISH] {} {} API | {} ms =========", className, method.getName(), totalTimeMillis);
-        return proceed;
-    }
-
-    @AfterThrowing(value = "all()", throwing = "exception")
-    public void logAfterThrowing(JoinPoint joinPoint, CustomException exception) {
-
-        try {
-
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-
-            String className = signature.getDeclaringType().getSimpleName();
-            Method method = signature.getMethod();
-
-            String[] parameterNames = signature.getParameterNames();
-            Object[] args = joinPoint.getArgs();
-
-            String returnArgs = null;
-
-            for (int i = 0; i < method.getParameters().length; i++) {
-                if (parameterNames[i] != null) {
-                    returnArgs += " | " + parameterNames[i] + " = ";
-                    returnArgs += args[i] + "  ";
+        for (int i = 0; i < method.getParameters().length; i++) {
+            if (parameterNames[i] != null) {
+                if (!parameterNames[i].equals("servletRequest")) {
+                    returnArgs.append(" | ").append(parameterNames[i]).append(" = ");
+                    returnArgs.append(args[i]).append(" ");
                 }
             }
+        }
 
-            log.error("[ERROR] {} | {} | throwing = {} | reqArgs = {}",
-                    className,
-                    method.getName(),
-                    exception.getExceptionCode().name(),
-                    returnArgs);
+        log.error("[ERROR] {} | {} | throwing = {} | reqArgs : {}",
+                className,
+                method.getName(),
+                exception.getExceptionCode().name(),
+                returnArgs);
 
-            if (exception.getExceptionCode().getHttpStatus().equals(INTERNAL_SERVER_ERROR)) {
+        if (exception.getExceptionCode().getHttpStatus().equals(INTERNAL_SERVER_ERROR)) {
 
-                if (exception.getException() != null) {
-                    Sentry.captureException(exception);
-                    log.error("##### SERVER ERROR DESCRIPTION #####", exception.getException());
-                }
+            if (exception.getException() != null) {
+                Sentry.captureException(exception);
+                log.error("##### SERVER ERROR DESCRIPTION #####", exception.getException());
             }
-        } catch (Exception e) {
-            throw new CustomException(e, AOP_LOGGING_ERROR);
         }
     }
 }
