ef4cd6c052dbcdf32af5faa22c40aabd6e72670a
nahyeon99 <nahyeonee99@gmail.com>
2023-09-11 05:04:33 +0900

    [Refactor] swagger 설정 수정 - 기존 springfox 에서 openAPI 의존성에 맞게 설정 수정

--- src/main/java/idorm/idormServer/config/SwaggerConfiguration.java
+++ src/main/java/idorm/idormServer/config/SwaggerConfiguration.java
@@ -1,34 +1,12 @@
 package idorm.idormServer.config;
 
-import com.fasterxml.classmate.TypeResolver;
-import idorm.idormServer.calendar.dto.Calendar.CalendarAdminResponseDto;
-import idorm.idormServer.calendar.dto.Calendar.CalendarDefaultResponseDto;
-import idorm.idormServer.calendar.dto.Team.TeamMemberFindResponseDto;
-import idorm.idormServer.calendar.dto.Team.TeamMemberManyFullResponseDto;
-import idorm.idormServer.calendar.dto.TeamCalendar.SleepoverCalendarAbstractResponseDto;
-import idorm.idormServer.calendar.dto.TeamCalendar.TeamCalendarAbstractResponseDto;
-import idorm.idormServer.calendar.dto.TeamCalendar.TeamCalendarDefaultResponseDto;
-import idorm.idormServer.community.dto.comment.CommentDefaultResponseDto;
-import idorm.idormServer.community.dto.comment.CommentParentResponseDto;
-import idorm.idormServer.community.dto.post.PostAbstractResponseDto;
-import idorm.idormServer.community.dto.post.PostOneResponseDto;
-import idorm.idormServer.email.dto.EmailDefaultResponseDto;
-
-import idorm.idormServer.matching.dto.MatchingDefaultResponseDto;
-import idorm.idormServer.matchingInfo.dto.MatchingInfoDefaultResponseDto;
-import idorm.idormServer.member.dto.MemberDefaultResponseDto;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import java.util.Arrays;
-import java.util.List;
 
 import static idorm.idormServer.config.SecurityConfiguration.AUTHENTICATION_HEADER_NAME;
 
@@ -36,62 +14,26 @@ import static idorm.idormServer.config.SecurityConfiguration.AUTHENTICATION_HEAD
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        TypeResolver typeResolver = new TypeResolver();
+    public OpenAPI openAPI() {
 
-        return new Docket(DocumentationType.OAS_30)
-                .additionalModels(
-                        typeResolver.resolve(EmailDefaultResponseDto.class),
-                        typeResolver.resolve(MemberDefaultResponseDto.class),
-                        typeResolver.resolve(MatchingInfoDefaultResponseDto.class),
-                        typeResolver.resolve(MatchingDefaultResponseDto.class),
-                        typeResolver.resolve(PostAbstractResponseDto.class),
-                        typeResolver.resolve(PostOneResponseDto.class),
-                        typeResolver.resolve(CommentDefaultResponseDto.class),
-                        typeResolver.resolve(CommentParentResponseDto.class),
-                        typeResolver.resolve(CalendarDefaultResponseDto.class),
-                        typeResolver.resolve(CalendarAdminResponseDto.class),
-                        typeResolver.resolve(TeamMemberFindResponseDto.class),
-                        typeResolver.resolve(TeamCalendarDefaultResponseDto.class),
-                        typeResolver.resolve(TeamCalendarAbstractResponseDto.class),
-                        typeResolver.resolve(SleepoverCalendarAbstractResponseDto.class),
-                        typeResolver.resolve(TeamMemberManyFullResponseDto.class)
-                )
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo()).securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()));
-    }
+        final Info info = new Info()
+                .title("아이돔 production api")
+                .description("운영 서버 api description")
+                .version("2.0.1");
 
-    private ApiKey apiKey() {
+        final String tokenSchemaName = AUTHENTICATION_HEADER_NAME;
+        final SecurityRequirement tokenRequirement = new SecurityRequirement().addList(tokenSchemaName);
+        final Components components = new Components()
+                .addSecuritySchemes(tokenSchemaName, new SecurityScheme()
+                        .name(tokenSchemaName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                );
 
-        return new ApiKey(AUTHENTICATION_HEADER_NAME, AUTHENTICATION_HEADER_NAME, "header");
-    }
-
-    private SecurityContext securityContext() {
-        return springfox
-                .documentation
-                .spi.service
-                .contexts
-                .SecurityContext
-                .builder()
-                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
-    }
-
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference(AUTHENTICATION_HEADER_NAME, authorizationScopes));
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("idorm API")
-                .version("1.0.0")
-                .build();
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(tokenRequirement)
+                .components(components);
     }
 }
