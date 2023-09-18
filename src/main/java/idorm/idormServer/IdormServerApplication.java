e9d8021c3fc4432e09299a9a71d6a4f23330ac3f
nahyeon99 <nahyeonee99@gmail.com>
2023-09-18 19:32:53 +0900

    [Chore] 테스트 서버 config 파일 경로 설정 - ec2 테스트 서버 내 yml 파일 경로 설정 - 파일명 변경에 따른 이그노어 수정

--- src/main/java/idorm/idormServer/IdormServerApplication.java
+++ src/main/java/idorm/idormServer/IdormServerApplication.java
@@ -1,7 +1,7 @@
 package idorm.idormServer;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,8 +10,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class IdormServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IdormServerApplication.class, args);
-	}
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:/home/ec2-user/idorm-test/application-develop.yml";
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(IdormServerApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
 }
