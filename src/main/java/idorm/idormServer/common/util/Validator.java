ed84e4c375ea1b286012e2139d4696cb3b7c0933
nahyeon99 <nahyeonee99@gmail.com>
2024-02-09 05:52:13 +0900

    [Feat] Validator length 검증 기능 추가

--- src/main/java/idorm/idormServer/common/util/Validator.java
+++ src/main/java/idorm/idormServer/common/util/Validator.java
@@ -4,6 +4,8 @@ import static idorm.idormServer.common.exception.ExceptionCode.FIELD_REQUIRED;
 
 import idorm.idormServer.common.exception.CustomException;
 import idorm.idormServer.common.exception.ExceptionCode;
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,38 @@ public final class Validator {
         }
     }
 
-    public static void validateNotNull(Object object, ExceptionCode exceptionCode) {
+    public static void validateNotBlank(List<String> values) {
+        values.forEach(Validator::validateNotBlank);
+    }
+
+    public static void validateNotNull(Object object) {
         if (object == null) {
+            throw new CustomException(null, FIELD_REQUIRED);
+        }
+    }
+
+    public static void validateNotNull(List<Object> objects) {
+        objects.forEach(Validator::validateNotNull);
+    }
+
+    public static void validateLength(String value, int min, int max, ExceptionCode exceptionCode) {
+        if (value.length() < min || value.length() > max) {
+            throw new CustomException(null, exceptionCode);
+        }
+    }
+
+    public static void validateSize(Integer value, int min, int max, ExceptionCode exceptionCode) {
+        if (value < min || value > max) {
+            throw new CustomException(null, exceptionCode);
+        }
+    }
+
+    public static void validateMaxLength(String value, int max, ExceptionCode exceptionCode) {
+        if (Objects.isNull(value)) {
+            return;
+        }
+
+        if (value.length() > max) {
             throw new CustomException(null, exceptionCode);
         }
     }
