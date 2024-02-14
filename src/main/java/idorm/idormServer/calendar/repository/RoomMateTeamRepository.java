b6087bce48e3b1c658abdc87499446eb19c9de08
nahyeon99 <nahyeonee99@gmail.com>
2024-02-15 05:49:55 +0900

    [Style] 불필요한 파일 제거

--- src/main/java/idorm/idormServer/calendar/repository/RoomMateTeamRepository.java
+++
@@ -1,7 +0,0 @@
-package idorm.idormServer.calendar.repository;
-
-import idorm.idormServer.calendar.domain.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomMateTeamRepository extends JpaRepository<Team, Long> {
-}
