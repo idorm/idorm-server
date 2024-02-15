9608ef180a5d271b187c3aff8d51fa28d9889c5f
nahyeon99 <nahyeonee99@gmail.com>
2024-02-15 20:58:34 +0900

    [Refactor] Report mapper 메서드 오버로딩

--- src/main/java/idorm/idormServer/report/adapter/out/persistence/ReportJpaEntity.java
+++ src/main/java/idorm/idormServer/report/adapter/out/persistence/ReportJpaEntity.java
@@ -1,7 +1,7 @@
 package idorm.idormServer.report.adapter.out.persistence;
 
-import idorm.idormServer.member.adapter.out.persistence.MemberJpaEntity;
 import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,6 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import idorm.idormServer.community.adapter.out.persistence.CommentJpaEntity;
+import idorm.idormServer.community.adapter.out.persistence.PostJpaEntity;
+import idorm.idormServer.member.adapter.out.persistence.MemberJpaEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,31 +25,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReportJpaEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "report_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "report_id")
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reported_member_id")
-    private MemberJpaEntity reportedMember;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reported_member_id")
+	private MemberJpaEntity reportedMember;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reporter_member_id")
-    private MemberJpaEntity reporterMember;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reporter_member_id")
+	private MemberJpaEntity reporterMember;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reported_post_id")
-    private Post reportedPost;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reported_post_id")
+	private PostJpaEntity reportedPost;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reported_comment_id")
-    private Comment reportedComment;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reported_comment_id")
+	private CommentJpaEntity reportedComment;
 
-    private Character reasonType;
+	private Character reasonType;
 
-    private String reason;
+	private String reason;
 
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
 }
--- src/main/java/idorm/idormServer/report/domain/Report.java
+++ src/main/java/idorm/idormServer/report/domain/Report.java
@@ -1,10 +1,12 @@
 package idorm.idormServer.report.domain;
 
-import idorm.idormServer.common.util.Validator;
-import idorm.idormServer.member.domain.Member;
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.xml.stream.events.Comment;
+
+import idorm.idormServer.common.util.Validator;
+import idorm.idormServer.community.domain.Comment;
+import idorm.idormServer.community.domain.Post;
+import idorm.idormServer.member.domain.Member;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,82 +15,82 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Report {
 
-    private Long id;
-    private Member reportedMember;
-    private Member reporterMember;
-    private Post reportedPost;
-    private Comment reportedComment;
-    private Character reasonType;
-    private String reason;
-    private LocalDateTime createdAt;
+	private Long id;
+	private Member reportedMember;
+	private Member reporterMember;
+	private Post reportedPost;
+	private idorm.idormServer.community.domain.Comment reportedComment;
+	private Character reasonType;
+	private String reason;
+	private LocalDateTime createdAt;
 
-    private Report(Member reporterMember,
-                   Member reportedMember,
-                   Post reportedPost,
-                   Comment reportedComment,
-                   Character reasonType,
-                   String reason) {
-        this.reporterMember = reporterMember;
-        this.reportedMember = reportedMember;
-        this.reportedPost = reportedPost;
-        this.reportedComment = reportedComment;
-        this.reasonType = reasonType;
-        this.reason = reason;
-        this.createdAt = LocalDateTime.now();
-    }
+	private Report(Member reporterMember,
+		Member reportedMember,
+		Post reportedPost,
+		Comment reportedComment,
+		Character reasonType,
+		String reason) {
+		this.reporterMember = reporterMember;
+		this.reportedMember = reportedMember;
+		this.reportedPost = reportedPost;
+		this.reportedComment = reportedComment;
+		this.reasonType = reasonType;
+		this.reason = reason;
+		this.createdAt = LocalDateTime.now();
+	}
 
-    public static Report memberReport(final Member reporterMember,
-                                      final Member reportedMember,
-                                      final MemberReason reasonType,
-                                      final String reason) {
-        Validator.validateNotNull(List.of(reportedMember, reporterMember, reasonType));
-        return new Report(reporterMember,
-                reportedMember,
-                null,
-                null,
-                reasonType.getType(),
-                reason);
-    }
+	public static Report memberReport(final Member reporterMember,
+		final Member reportedMember,
+		final MemberReason reasonType,
+		final String reason) {
+		Validator.validateNotNull(List.of(reportedMember, reporterMember, reasonType));
+		return new Report(reporterMember,
+			reportedMember,
+			null,
+			null,
+			reasonType.getType(),
+			reason);
+	}
 
-    public static Report postReport(final Member reporterMember,
-                                    final Member reportedMember,
-                                    final Post reportedPost,
-                                    final CommunityReason reasonType,
-                                    final String reason) {
+	public static Report postReport(final Member reporterMember,
+		final Member reportedMember,
+		final Post reportedPost,
+		final CommunityReason reasonType,
+		final String reason) {
 
-        Validator.validateNotNull(List.of(reportedMember, reporterMember, reportedPost, reasonType));
-        return new Report(reporterMember,
-                reportedMember,
-                reportedPost,
-                null,
-                reasonType.getType(),
-                reason);
-    }
+		Validator.validateNotNull(List.of(reportedMember, reporterMember, reportedPost, reasonType));
+		return new Report(reporterMember,
+			reportedMember,
+			reportedPost,
+			null,
+			reasonType.getType(),
+			reason);
+	}
 
-    public static Report commentReport(final Member reporterMember,
-                                    final Member reportedMember,
-                                    final Comment reportedComment,
-                                    final CommunityReason reasonType,
-                                    final String reason) {
+	public static Report commentReport(final Member reporterMember,
+		final Member reportedMember,
+		final Comment reportedComment,
+		final CommunityReason reasonType,
+		final String reason) {
 
-        Validator.validateNotNull(List.of(reportedMember, reporterMember, reportedComment, reasonType));
-        return new Report(reporterMember,
-                reportedMember,
-                null,
-                reportedComment,
-                reasonType.getType(),
-                reason);
-    }
+		Validator.validateNotNull(List.of(reportedMember, reporterMember, reportedComment, reasonType));
+		return new Report(reporterMember,
+			reportedMember,
+			null,
+			reportedComment,
+			reasonType.getType(),
+			reason);
+	}
 
-    public static Report forMapper(final Long id,
-                                   final Member reportedMember,
-                                   final Member reporterMember,
-                                   final Post reportedPost,
-                                   final Comment reportedComment,
-                                   final Character reasonType,
-                                   final String reason,
-                                   final LocalDateTime createdAt) {
-        return new Report(id, reportedMember, reporterMember, reportedPost, reportedComment, reasonType, reason,
-                createdAt);
-    }
+	public static Report forMapper(final Long id,
+		final Member reportedMember,
+		final Member reporterMember,
+		final Post reportedPost,
+		final Comment reportedComment,
+		final Character reasonType,
+		final String reason,
+		final LocalDateTime createdAt) {
+		return new Report(id, reportedMember, reporterMember, reportedPost, reportedComment, reasonType, reason,
+			createdAt);
+	}
 }
