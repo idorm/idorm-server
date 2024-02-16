6998fe2739ff032f7837e0ce62cf8a74d1197e94
nahyeon99 <nahyeonee99@gmail.com>
2024-02-17 06:25:14 +0900

    8. ErrorResponse(HttpStatus -> int 로 타입캐스팅 추가), SuccessResponse(코드포맷팅) 수정

--- src/main/java/idorm/idormServer/common/response/ErrorResponse.java
+++ src/main/java/idorm/idormServer/common/response/ErrorResponse.java
@@ -1,36 +1,34 @@
 package idorm.idormServer.common.response;
 
+import org.springframework.http.HttpStatus;
+
 import idorm.idormServer.common.exception.BaseResponseCode;
-import idorm.idormServer.common.exception.GlobalResponseCode;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @ToString
 public class ErrorResponse extends BaseResponse {
 
-  private final HttpStatus status;
+	private final Integer status;
 
-  @Builder
-  public ErrorResponse(String responseCode, String responseMessage, HttpStatus status) {
-    super(false, responseCode, responseMessage);
-    this.status = status;
-  }
+	@Builder
+	public ErrorResponse(String responseCode, String responseMessage, HttpStatus status) {
+		super(false, responseCode, responseMessage);
+		this.status = Integer.valueOf(status.value());
+	}
 
-  public static ErrorResponse of(BaseResponseCode code) {
-    return ErrorResponse.builder()
-        .responseCode(code.getName())
-        .responseMessage(code.getMessage())
-        .status(code.getStatus())
-        .build();
-  }
+	public static ErrorResponse of(BaseResponseCode code) {
+		return ErrorResponse.builder()
+			.responseCode(code.getName())
+			.responseMessage(code.getMessage())
+			.status(code.getStatus())
+			.build();
+	}
 
-
-  public static ErrorResponse of(BaseResponseCode code, String message) {
-    return ErrorResponse.builder().responseCode(code.getName())
-        .responseMessage(code.getMessage()).status(code.getStatus()).build();
-  }
-
+	public static ErrorResponse of(BaseResponseCode code, String message) {
+		return ErrorResponse.builder().responseCode(code.getName())
+			.responseMessage(code.getMessage()).status(code.getStatus()).build();
+	}
 }
