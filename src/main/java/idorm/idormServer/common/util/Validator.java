c565233c11b9c84c597b16d7c8df5ccddeda1ff4
shyeon4643 <songhyeon4643@gmail.com>
2024-02-17 17:58:52 +0900

    [Fix] 전역 Validator 기존 예외 파라미터 전달 오류 수정

--- src/main/java/idorm/idormServer/common/util/Validator.java
+++ src/main/java/idorm/idormServer/common/util/Validator.java
@@ -1,9 +1,8 @@
 package idorm.idormServer.common.util;
 
-import static idorm.idormServer.common.exception.ExceptionCode.FIELD_REQUIRED;
-
-import idorm.idormServer.common.exception.CustomException;
-import idorm.idormServer.common.exception.ExceptionCode;
+import idorm.idormServer.common.exception.BaseException;
+import idorm.idormServer.common.exception.BaseResponseCode;
+import idorm.idormServer.common.exception.FieldRequiredException;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
@@ -13,51 +12,53 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public final class Validator {
 
-    public static void validateNotBlank(String value) {
-        if (value == null || value.isBlank()) {
-            throw new CustomException(null, FIELD_REQUIRED);
-        }
+  public static void validateNotBlank(String value) {
+    if (value == null || value.isBlank()) {
+      throw new FieldRequiredException();
+    }
+  }
+
+  public static void validateNotBlank(List<String> values) {
+    values.forEach(Validator::validateNotBlank);
+  }
+
+  public static void validateNotNull(Object object) {
+    if (object == null) {
+      throw new FieldRequiredException();
+    }
+  }
+
+  public static void validateNotNull(List<Object> objects) {
+    objects.forEach(Validator::validateNotNull);
+  }
+
+  public static void validateLength(String value, int min, int max,
+      BaseResponseCode baseResponseCode) {
+    if (value.length() < min || value.length() > max) {
+      throw new BaseException(baseResponseCode);
+    }
+  }
+
+  public static void validateSize(Integer value, int min, int max,
+      BaseResponseCode baseResponseCode) {
+    if (value < min || value > max) {
+      throw new BaseException(baseResponseCode);
+    }
+  }
+
+  public static void validateMaxLength(String value, int max, BaseResponseCode baseResponseCode) {
+    if (Objects.isNull(value)) {
+      return;
     }
 
-    public static void validateNotBlank(List<String> values) {
-        values.forEach(Validator::validateNotBlank);
+    if (value.length() > max) {
+      throw new BaseException(baseResponseCode);
     }
+  }
 
-    public static void validateNotNull(Object object) {
-        if (object == null) {
-            throw new CustomException(null, FIELD_REQUIRED);
-        }
+  public static void validateFormat(String value, String regex, BaseResponseCode baseResponseCode) {
+    if (!Pattern.matches(regex, value)) {
+      throw new BaseException(baseResponseCode);
     }
-
-    public static void validateNotNull(List<Object> objects) {
-        objects.forEach(Validator::validateNotNull);
-    }
-
-    public static void validateLength(String value, int min, int max, ExceptionCode exceptionCode) {
-        if (value.length() < min || value.length() > max) {
-            throw new CustomException(null, exceptionCode);
-        }
-    }
-
-    public static void validateSize(Integer value, int min, int max, ExceptionCode exceptionCode) {
-        if (value < min || value > max) {
-            throw new CustomException(null, exceptionCode);
-        }
-    }
-
-    public static void validateMaxLength(String value, int max, ExceptionCode exceptionCode) {
-        if (Objects.isNull(value)) {
-            return;
-        }
-
-        if (value.length() > max) {
-            throw new CustomException(null, exceptionCode);
-        }
-    }
-
-    public static void validateFormat(String value, String regex, ExceptionCode exceptionCode) {
-        if (!Pattern.matches(regex, value)) {
-            throw new CustomException(null, exceptionCode);
-        }
-    }
+  }
 }
