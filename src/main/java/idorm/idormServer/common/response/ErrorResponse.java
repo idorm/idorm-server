f5600fdab7b85001f9ad0d14a94acd77aee5395a
nahyeon99 <nahyeonee99@gmail.com>
2024-02-27 03:02:57 +0900

    [Fix] 에러 응답 구조 수정

--- src/main/java/idorm/idormServer/common/response/ErrorResponse.java
+++ src/main/java/idorm/idormServer/common/response/ErrorResponse.java
@@ -2,31 +2,22 @@ package idorm.idormServer.common.response;
 
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import idorm.idormServer.common.exception.BaseResponseCode;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
 
-@Getter
-@ToString
-public class ErrorResponse {
-
-	private final String responseCode;
-	private final String responseMessage;
-	private final HttpStatus status;
+public record ErrorResponse(String responseCode, String responseMessage, @JsonIgnore HttpStatus status) {
 
 	@Builder
-	public ErrorResponse(String responseCode, String responseMessage, HttpStatus status) {
-		this.responseCode = responseCode;
-		this.responseMessage = responseMessage;
-		this.status = status;
+	public ErrorResponse {
 	}
 
 	public static ErrorResponse of(BaseResponseCode code) {
 		return ErrorResponse.builder()
+			.status(code.getStatus())
 			.responseCode(code.getName())
 			.responseMessage(code.getMessage())
-			.status(code.getStatus())
 			.build();
 	}
 
