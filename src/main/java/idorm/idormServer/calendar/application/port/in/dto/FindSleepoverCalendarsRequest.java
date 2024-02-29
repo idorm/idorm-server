72bdec5df647b2f9a287ba67fc03266d498b5d35
shyeon4643 <songhyeon4643@gmail.com>
2024-03-01 06:08:10 +0900

    [Fix] 캘린더 요청 DTO 수정

--- src/main/java/idorm/idormServer/calendar/application/port/in/dto/FindSleepoverCalendarsRequest.java
+++ src/main/java/idorm/idormServer/calendar/application/port/in/dto/FindSleepoverCalendarsRequest.java
@@ -1,18 +1,10 @@
 package idorm.idormServer.calendar.application.port.in.dto;
 
-import java.time.YearMonth;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.YearMonth;
+import javax.validation.constraints.NotNull;
 
 public record FindSleepoverCalendarsRequest(
-    @NotNull(message = "내용을 입력해 주세요.")
-    @Positive(message = "조회할 회원 식별자는 양수만 가능합니다.")
-    Long memberId,
-
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM")
     YearMonth yearMonth
--- src/main/java/idorm/idormServer/calendar/application/port/in/dto/SaveTeamCalendarRequest.java
+++ src/main/java/idorm/idormServer/calendar/application/port/in/dto/SaveTeamCalendarRequest.java
@@ -1,42 +1,38 @@
 package idorm.idormServer.calendar.application.port.in.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Set;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import idorm.idormServer.calendar.entity.Duration;
 import idorm.idormServer.calendar.entity.Participant;
 import idorm.idormServer.calendar.entity.Participants;
 import idorm.idormServer.calendar.entity.Period;
 import idorm.idormServer.calendar.entity.Team;
 import idorm.idormServer.calendar.entity.TeamCalendar;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Set;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public record SaveTeamCalendarRequest(
-	@NotBlank(message = "내용을 입력해 주세요.")
-	@Size(max = 15, message = "제목은 ~15자 이내여야 합니다.")
-	String title,
-	@Size(max = 100, message = "내용은 ~100자 이내여야 합니다.")
-	String content,
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-	LocalDate startDate,
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-	LocalDate endDate,
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
-	LocalTime startTime,
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
-	LocalTime endTime,
-	Set<Participant> participants
+    @NotBlank(message = "내용을 입력해 주세요.")
+    @Size(max = 15, message = "제목은 ~15자 이내여야 합니다.")
+    String title,
+    @Size(max = 100, message = "내용은 ~100자 이내여야 합니다.")
+    String content,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    LocalDate startDate,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    LocalDate endDate,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    LocalTime startTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    LocalTime endTime,
+    Set<Participant> participants
 ) {
-	public TeamCalendar toEntity(final Team team) {
-		return new TeamCalendar(title, content, new Period(startDate, endDate), new Duration(startTime, endTime),
-			new Participants(participants), team
-		);
-	}
+
+  public TeamCalendar toEntity(final Team team) {
+    return new TeamCalendar(title, content, new Period(startDate, endDate), new Duration(startTime, endTime),
+        new Participants(participants), team
+    );
+  }
 }
