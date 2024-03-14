37153a7dcb0f858ebe533a38b1492e0f1e7c4a47
shyeon4643 <songhyeon4643@gmail.com>
2024-03-14 18:18:15 +0900

    [Fix] 공감 기능 비관적 락 구현 방식 변경
    * 변경 사유 : @Lock 미작동 버그로 인한 네이티브 쿼리 임시 변경

--- src/main/java/idorm/idormServer/community/post/application/port/out/LoadPostPort.java
+++ src/main/java/idorm/idormServer/community/post/application/port/out/LoadPostPort.java
@@ -1,24 +1,24 @@
 package idorm.idormServer.community.post.application.port.out;
 
+import idorm.idormServer.community.post.entity.Post;
+import idorm.idormServer.matchingInfo.entity.DormCategory;
 import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import idorm.idormServer.community.post.entity.Post;
-import idorm.idormServer.matchingInfo.entity.DormCategory;
-
 public interface LoadPostPort {
 
-	Post findById(Long postId);
-	Post findByIdAndMemberId(Long postId, Long memberId);
-	Post findByIdWithPessimisticWrite(Long postId);
+  Post findById(Long postId);
 
-	Page<Post> findPostsByDormCategoryAndIsDeletedFalse(DormCategory dormCategory, Pageable pageable);
+  Post findByIdAndMemberId(Long postId, Long memberId);
 
-	List<Post> findTopPostsByDormCateogry(DormCategory dormCategory);
+  Post findByPostIdWithLock(Long postId);
 
-	Post findTopPostByDormCategory(DormCategory dormCategory);
+  Page<Post> findPostsByDormCategoryAndIsDeletedFalse(DormCategory dormCategory, Pageable pageable);
 
-	List<Post> findPostsByMemberId(Long memberId);
+  List<Post> findTopPostsByDormCateogry(DormCategory dormCategory);
+
+  Post findTopPostByDormCategory(DormCategory dormCategory);
+
+  List<Post> findPostsByMemberId(Long memberId);
 }
