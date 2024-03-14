8700f96082ac80c327169b3b87ab965cb6b4a10c
Songhyeon Seo <songhyeon4643@gmail.com>
2024-03-14 18:28:37 +0900

    [Test] 게시글 공감 동시성 테스트 코드 작성 (#47)
    * [Test] 동시성 환경에서 공감 기능 테스트 케이스 추가
    
    * [Fix] 공감 기능 비관적 락 구현 방식 변경
    
    * 변경 사유 : @Lock 미작동 버그로 인한 네이티브 쿼리 임시 변경

--- src/main/java/idorm/idormServer/community/post/adapter/out/persistence/PostRepository.java
+++ src/main/java/idorm/idormServer/community/post/adapter/out/persistence/PostRepository.java
@@ -1,6 +1,9 @@
 package idorm.idormServer.community.post.adapter.out.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import idorm.idormServer.community.post.entity.Post;
@@ -8,5 +11,6 @@ import idorm.idormServer.community.post.entity.Post;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-	Post findByIdAndIsDeletedIsFalse(Long id);
+	@Query(value = "SELECT * FROM post WHERE post_id = :id FOR UPDATE", nativeQuery = true)
+	Optional<Post> findByPostIdWithLock(@Param("id") Long id);
 }
